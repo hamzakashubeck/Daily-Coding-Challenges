b33fc06950a45c8784dd418c4df9fa58e65965b1
hamzakashubeck <93697670+hamzakashubeck@users.noreply.github.com>
2021-11-04 11:54:26 -0400

    Create Challenge1.java

---
+++ Challenge1.java
@@ -0,0 +1,24 @@
+//Challenge #1: Create a program that will ask the users name, age, and reddit username.
+//Have it tell them the information back, in the format: your name is (blank), you are (blank) years old, and your username is (blank)
+
+import java.util.Scanner;
+
+public class Challenge1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String name = in.nextLine();
+
+        System.out.println("How old are you?");
+        int age = Integer.parseInt(in.nextLine());
+
+        System.out.println("What is your Reddit username?");
+        String user = in.nextLine();
+
+        System.out.println("your name is " + name + ", you are " + age
+                + " years old, and your username is " + user);
+    }
+
+}
